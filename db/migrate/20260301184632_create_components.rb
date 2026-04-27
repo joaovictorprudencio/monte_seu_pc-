49ff65fc969113a490895c83352e6f87b01119bb
joaovictorprudencio <joaovprudencio13@gmail.com>
2026-04-27 13:50:52 -0300

    ajustes de atributos

--- db/migrate/20260301184632_create_components.rb
+++ db/migrate/20260301184632_create_components.rb
@@ -3,7 +3,6 @@ class CreateComponents < ActiveRecord::Migration[8.1]
     create_table :components do |t|
       t.string :name
       t.string :brand
-      t.references :computer, null: false, foreign_key: true
       t.string :category
       t.string :architecture
       t.decimal :price, precision: 10, scale: 2
