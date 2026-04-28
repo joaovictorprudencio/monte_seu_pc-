ecb8e24e6f977df017d0624d7fe526dc31ad3543
joaovictorprudencio <joaovprudencio13@gmail.com>
2026-04-27 22:25:42 -0300

    active store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_04_27_140833) do
+ActiveRecord::Schema[8.1].define(version: 2026_04_27_192728) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -63,6 +63,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_04_27_140833) do
 
   create_table "computers", force: :cascade do |t|
     t.datetime "created_at", null: false
+    t.jsonb "data", default: {}
     t.string "description"
     t.string "name"
     t.decimal "total_price", precision: 10, scale: 2
