4d9580245b7386e7c6a3f8a139f7bc6a6bd16f2d
joaovictorprudencio <joaovprudencio13@gmail.com>
2026-05-02 15:57:18 -0300

    criando metodos principais de manipulação de montagem

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -14,12 +14,34 @@ class Computer < ApplicationRecord
   end
 
 
-  def create_moutinng()
-
+  def create_moutinng(componets) 
+    componets.each do |componente |
+    computer_parts.create(component_id: componente.id,computer_id: self.id)
+    caclculate_total_price(componente.price)
+    end 
   end
 
- 
 
+  def calculate_total_price(price)
+    update(total_price: total_price + price)
+  end
 
   
-end
+
+  def change_part (componente, new_componente)
+    computer_part = computer_parts.find_by(component_id: componente.id)
+
+    unless computer_part
+      raise ActiveRecord::RecordNotFound, "Componente não encontrado na máquina"
+    end
+
+    new_price = self.price - component.price + new_componente.price
+    update(total_price: new_price)
+
+    computer_part.update(component_id: new_componente.id)
+  end
+
+  
+
+  
+end 
