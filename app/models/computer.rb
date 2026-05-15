ecbd280f6c6f69a05d676997feec3bced5ed46fa
joaovictorprudencio <joaovprudencio13@gmail.com>
2026-05-15 18:14:28 -0300

    ajustes de nomeclatura e estrutura de  metodos

--- app/models/computer.rb
+++ app/models/computer.rb
@@ -1,26 +1,22 @@
 class Computer < ApplicationRecord
-
-   has_many :computer_parts
-   has_many :components, through: :computer_parts
-
+  has_many :computer_parts
+  has_many :components, through: :computer_parts
 
   def total_price=(value)
     if value.is_a?(String) && value.include?(",")
-      clean_value = value.gsub(/[R$\s.]/, '').gsub(',', '.')
+      clean_value = value.gsub(/[R$\s.]/, "").gsub(",", ".")
       super(clean_value)
     else
       super(value)
     end
   end
 
-
-  def create_moutinng(componets) 
-    componets.each do |componente |
-    computer_parts.create(component_id: componente.id,computer_id: self.id)
-    caclculate_total_price(componente.price)
-    end 
+  def create_mounting(components_list)
+    components_list.each do |component|
+      computer_parts.create(component_id: component.id, computer_id: id)
+      calculate_total_price(component.price)
+    end
   end
-
 
   def calculate_total_price(price)
     update(total_price: total_price + price)
@@ -39,14 +35,15 @@ class Computer < ApplicationRecord
     update(total_price: new_price)
 
     computer_part.update(component_id: new_componente.id)
+
   end
 
-
   def validate_compatibility(component)
- 
-    motherboard = self.computer_parts.findy_by(category: "MOTHERBOARD")
-    cpu = self.computer_parts.find_by(category: "CPU")
-    
+    motherboard = components.find_by(category: "MOTHERBOARD")
+    cpu = components.find_by(category: "CPU")
+    ram = components.find_by(category: "RAM")
+    case_component = components.find_by(category: "CASE") 
+
     case component.category
     when "CPU"
      
@@ -54,61 +51,76 @@ class Computer < ApplicationRecord
          raise ActiveRecord::RecordNotFound, "Placa-mãe não encontrada"
       end
 
-      if  motherboard.socket != component.socket || motherboard.arvhitecture != component.architecture 
+      if motherboard.socket != component.socket || motherboard.architecture != component.architecture
         raise "Incompatibilidade: A CPU não é compatível com a placa-mãe."
       end
 
     when "MOTHERBOARD"
+      raise ActiveRecord::RecordNotFound, "Placa-mãe não encontrada" unless motherboard
 
-      unless motherboard
-        if component.socket != cpu.socket || component.architecture != cpu.architecture
+      if component.socket != cpu.socket || component.architecture != cpu.architecture
           raise "Incompatibilidade: A placa-mãe não é compatível com Processador."
-        end
       end
 
-      if cpu.socket != component.socket
-        raise "Incompatibilidade: A placa-mãe não é compatível com a CPU."
+      if component.ram_type != ram.ram_type 
+        raise "Incompatibilidade: A placa-mãe não é compatível com a RAM."
+      end
+
+      if component.slots < ram.slots
+        raise "Incompatibilidade: A placa-mãe não tem slots suficientes para a RAM."
       end
 
     when "RAM"
-      if motherboard && motherboard.ram_type != component.ram_type
+    unless motherboard
+      
+    end
+
+
+     if motherboard && motherboard.ram_type != component.ram_type
         raise "Incompatibilidade: O tipo de RAM não é compatível com a placa-mãe."
       end
+
+
+    if motherboard.slots < component.slots
+      raise "Incompatibilidade: A placa-mãe não tem slots suficientes para a RAM."
     end
 
   when "CASE"
-    unless motherboard
-      if component.form_factor != motherboard.form_factor
+    unless case_component
+        raise ActiveRecord::RecordNotFound, "Gabinete não encontrado"
+    end
+
+      if component.form_factor != motherboard.form_factor 
         raise "Incompatibilidade: O gabinete não é compatível com a placa-mãe."
       end
-    end
   end
 
   end
 
   def save_hash(component)
-    self.data = {
+    data = {
       name: self.name,
-      description: self.description,
-      type_of_use: self.type_of_use,
-      total_price: self.total_price,
+      description: description,
+      type_of_use: type_of_use,
+      total_price: total_price,
     }
 
     case component.category
     when "CPU"
-      self.data[:CPU] = component.name
+      data[:CPU] = component.name
     when "MOTHERBOARD"
-      cpu = component.findy_by(component_id: component.id)
-      self.data[:MOTHERBOARD] = component.name  
+      data[:MOTHERBOARD] = component.name  
     when "GPU"
-      self.data[:GPU] = component.name
+      data[:GPU] = component.name
     when "SOURCE"
-      self.data[:SOURCE] = component.name
+      data[:SOURCE] = component.name
     when "STORAGE"
-      self.data[:STORAGE] = component.name
+      data[:STORAGE] = component.name
     when "RAM"
-      self.data[:RAM] = component.name
+      data[:RAM] = component.name
     end  
+
+    save
   end
 
 
