b17db7ccbaf9fee74d03c6f3583faadb86d322ca
joaovictorprudencio <joaovprudencio13@gmail.com>
2026-05-24 17:29:53 -0300

    refactor: trocando herença de validator para EachValidator para melhor usabilidade

--- app/models/validators/compatibility_validate.rb
+++ app/models/validators/compatibility_validate.rb
@@ -1,50 +1,51 @@
-class CompatibilityValidator < ActiveModel::Validator
- def validate_compatibility_part(component)
-    motherboard = components.find_by(category: "MOTHERBOARD")
-    cpu = components.find_by(category: "CPU")
-    ram = components.find_by(category: "RAM")
-    case_component = components.find_by(category: "CASE") 
+class CompatibilityValidator < ActiveModel::EachValidator 
+
+ def validate_each(record, attribute, value)
+    motherboard = record.components.find_by(category: "MOTHERBOARD")
+    cpu = record.components.find_by(category: "CPU")
+    ram = record.components.find_by(category: "RAM")
+    case_component = record.components.find_by(category: "CASE") 
 
 
-    if component && component.cagegory.nil?
-        error.add("componente não encontrado")
+    if value.category.nil?
+        errors.add(:base,"componente não encontrado")
     end
   
 
-  case component.category
+  case value.category
     when "CPU"
     
-      if motherboard.socket != component.socket || motherboard.architecture != component.architecture
-        errors.add("Incompatibilidade: A CPU não é compatível com a placa-mãe.") 
+      if motherboard.socket != value.socket || motherboard.architecture != value.architecture
+        errors.add(:base,"Incompatibilidade: A CPU não é compatível com a placa-mãe.") 
       end
 
     when "MOTHERBOARD"
 
-      if component.socket != cpu.socket || component.architecture != cpu.architecture 
-          errors.add("Incompatibilidade: A placa-mãe não é compatível com Processador.") 
+      if value.socket != cpu.socket || value.architecture != cpu.architecture 
+          errors.add(:base,"Incompatibilidade: A placa-mãe não é compatível com Processador.") 
       end
 
-      if component.ram_type != ram.ram_type 
-         errors.add( "Incompatibilidade: A placa-mãe não é compatível com Processador.") 
+      if value.ram_type != ram.ram_type 
+         errors.add( :base, "Incompatibilidade: A placa-mãe não é compatível com Processador.") 
       end
 
-      if component.slots < ram.slots
-        errors.add("Incompatibilidade: A placa-mãe não tem slots suficientes para a RAM.")
+      if value.slots < ram.slots
+        errors.add(:base,"Incompatibilidade: A placa-mãe não tem slots suficientes para a RAM.")
       end
 
     when "RAM"
 
-    if  motherboard.ram_type != component.ram_type
-        errors.add("Incompatibilidade: O tipo de RAM não é compatível com a placa-mãe.") 
+    if  motherboard.ram_type != value.ram_type
+        errors.add(:base,"Incompatibilidade: O tipo de RAM não é compatível com a placa-mãe.") 
     end
 
-    if motherboard.slots < component.slots
-      errors.add("A placa-mãe não tem mais slots suficientes para a RAM.") 
+    if motherboard.slots < value.slots
+      errors.add(:base, "A placa-mãe não tem mais slots suficientes para a RAM.") 
     end
 
   when "CASE"
-      if component.form_factor != motherboard.form_factor 
-         errors.add("Incompatibilidade: O gabinete não é compatível com a placa-mãe.") 
+      if value.form_factor != motherboard.form_factor 
+         errors.add(:base, "Incompatibilidade: O gabinete não é compatível com a placa-mãe.") 
       end
   end
  end
