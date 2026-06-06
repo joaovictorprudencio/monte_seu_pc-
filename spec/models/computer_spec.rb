e4ab5e3eabe552322acc5611ac5618b70615091e
joaovictorprudencio <joaovprudencio13@gmail.com>
2026-06-06 15:51:43 -0300

    criando testes de validações do pc

--- spec/models/computer_spec.rb
+++ spec/models/computer_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Computer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "validação de instancia computer" do
+
+    it 'pc valido' do
+      p = build(:computer)
+      expect(p).to be_valid
+    end
+
+    it 'pc sem o preço ' do
+      p = build(:computer, total_price: nil)
+      expect(p).not_to be_valid
+      expect(p.errors[:total_price]).to include("can't be blank")
+    end
+
+  end
+
 end
+ 
