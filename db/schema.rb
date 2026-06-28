42aedd1a560261071e52f1ea83f53a8c475b6015
joaovictorprudencio <joaovprudencio13@gmail.com>
2026-06-28 17:42:31 -0300

    adicionando atributo ram_speed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_05_03_162517) do
+ActiveRecord::Schema[8.1].define(version: 2026_06_07_185218) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -51,6 +51,7 @@ ActiveRecord::Schema[8.1].define(version: 2026_05_03_162517) do
     t.integer "max_gpu_length"
     t.string "name"
     t.decimal "price", precision: 10, scale: 2
+    t.string "ram_speed"
     t.string "ram_type"
     t.integer "slots"
     t.string "socket"
