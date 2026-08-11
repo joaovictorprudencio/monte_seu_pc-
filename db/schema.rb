84b0e1a257de17b2b621cc4ec60a6b2d2daf739a
joaovictorprudencio <joaovprudencio13@gmail.com>
2026-08-10 21:51:56 -0300

    feature; criando model user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_06_07_185218) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_03_161200) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -73,9 +73,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_07_185218) do
     t.jsonb "data", default: {}
     t.string "description"
     t.string "name"
+    t.integer "status"
     t.decimal "total_price", precision: 10, scale: 2
     t.string "type_of_use"
     t.datetime "updated_at", null: false
+    t.bigint "user_id", null: false
+    t.index ["user_id"], name: "index_computers_on_user_id"
   end
 
   create_table "testes", force: :cascade do |t|
@@ -84,8 +87,18 @@ ActiveRecord::Schema[8.1].define(version: 2026_06_07_185218) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.string "password_digest"
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "computer_parts", "components"
   add_foreign_key "computer_parts", "computers"
+  add_foreign_key "computers", "users"
 end
