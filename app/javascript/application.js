f591934e38c7d626d0fe846ca033f4742affb8bd
joaovictorprudencio <joaovprudencio13@gmail.com>
2026-08-24 21:56:04 -0300

    correções no modulo de montagem

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,7 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-//= require jquery
-//= require xray
 import "@hotwired/turbo-rails"
 import "controllers"
 import "bootstrap"
+import "jquery"
 
